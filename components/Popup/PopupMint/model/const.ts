c2b37a9b017e9aa6220ef91684b88aa1b010fff8
Meerch <khukhriansk333@gmail.com>
2022-11-28 04:00:33 +0300

    change texts

---
+++ components/Popup/PopupMint/model/const.ts
@@ -0,0 +1,4 @@
+export const title = 'Fell the pAIn'
+export const textButtonMint = 'GET pAIn'
+export const description = 'You’re about to mint an *pAIn type* NFT. Think twice before you do it as it’s going to hurt. A LOT!'
+export const disclaimer = '(Disclaimer.These numbers will be recorded into your NFT’s metadata. They are approximate and may change as they’re derived from Chailink price feed at the moment of transaction confirmation)'
---
+++ components/Popup/PopupSuccess/const.ts
@@ -0,0 +1,11 @@
+export const textsForPopupSuccess = [
+    'It hurts terribly, but this pAIn makes you feel alive.',
+    'Your nervous system must hate you now.',
+    'Wasting money on distorted JPEGS wasn’t the best idea.',
+    'Probably, your mum doesn\'t love you.',
+    'You’ll get a neurogenic shock, if you keep minting.',
+    'You must be a pervy!',
+    'Did all those rugpulls make you pain-resistant?',
+    'Call the ambulance, if you still can.',
+    'It feels like running a marathon over a trail of scattered lego pieces barefooted.'
+]
