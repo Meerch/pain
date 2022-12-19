aa85c31891cff4d3fe8d756e0acf9e534aa02a5b
Meerch <khukhriansk333@gmail.com>
2022-12-20 00:02:21 +0300

    fix

--- components/Popup/PopupMint/model/useMintProcess.ts
+++ components/Popup/PopupMint/model/useMintProcess.ts
@@ -179,7 +179,7 @@ export const useMintProcess = () => {
             }
         })
 
-        if (ids && ids.length === 0) {
+        if (ids && ids.length !== 0) {
             dispatch(popupActions.setAmountMintedNfts(ids))
             dispatch(popupActions.changeCurrentPopup('success'))
         } else {
