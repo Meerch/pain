56cb880c591f24b9c163ecac1198b1c7ae317cdc
Meerch <56489774+Meerch@users.noreply.github.com>
2022-12-22 00:03:09 +0300

    Update constants.ts

--- components/shared/SocialLinks/constants.ts
+++ components/shared/SocialLinks/constants.ts
@@ -2,16 +2,16 @@ export const links = [
     {
         icon: '/images/social-links/twitter-4x.png',
         name: 'twitter',
-        href: 'https://twitter.com',
+        href: 'https://twitter.com/paintozero',
     },
     {
         icon: '/images/social-links/discord-4x.png',
         name: 'discord',
-        href: 'https://discord.com',
+        href: 'https://discord.gg/jfBYj7DrDR',
     },
     {
         icon: '/images/social-links/telegram-4x.png',
         name: 'telegram',
-        href: 'https://t.me',
+        href: 'https://t.me/neuralpepe',
     },
 ]
